9a60694f09d8abb2e0b9f78774758cfe0fe8c791
tplesetz <12537305+tplesetz@users.noreply.github.com>
2022-09-23 14:53:17 -0500

    rough draft script to pull script files from MEM

---
+++ exportIntuneScripts.ps1
@@ -0,0 +1,24 @@
+﻿$module = Get-Module -Name Microsoft.Graph.Intune
+if (-not $module)
+{
+    Install-Module NuGet -Force
+    Install-Module Microsoft.Graph.Intune
+}
+Import-Module Microsoft.Graph.Intune -Global
+
+Connect-MSGraph
+
+
+$FolderPath = "c:\temp\"
+    
+
+$deviceManagementScripts = Invoke-MSGraphRequest -Url "https://graph.microsoft.com/beta/deviceManagement/deviceManagementScripts"                               
+
+$scriptDetails = $deviceManagementScripts.value | Select-Object id, displayname, filename
+
+foreach($dmscript in $scriptDetails){
+            $script = Invoke-MSGraphRequest -Url "https://graph.microsoft.com/beta/deviceManagement/deviceManagementScripts/$($dmscript.id)" -HttpMethod GET
+            [System.Text.Encoding]::ASCII.GetString([System.Convert]::FromBase64String($($script.scriptContent))) | Out-File -Encoding ASCII -FilePath $(Join-Path $FolderPath $($script.fileName))
+        }
+
+       $scriptDetails | Out-File C:\temp\log.txt
